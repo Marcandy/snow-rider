d13bd24fb363ca7c78f6c15b8afe0ddc29b76350
marcandy <marcandy.nj@gmail.com>
2016-12-26 22:38:12 -0700

    jumbo

--- dist/views/bundle.js
+++ dist/views/bundle.js
@@ -14,6 +14,6 @@ angular.module('snowrider', ['ui.router']).config(function ($stateProvider, $url
 'use strict';
 
 angular.module('snowrider').controller('jumboCtrl', function ($scope, $sce) {
-  // $scope.vid = $sce.trustAsResourceUrl('../img/jumbo.mp4');
+  $scope.vid = $sce.trustAsResourceUrl('../img/jumbo.mp4');
 });
 //# sourceMappingURL=bundle.js.map

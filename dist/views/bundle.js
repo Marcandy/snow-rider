4715b0c972d86743e6a342af387dafa2de774905
marcandy <marcandy.nj@gmail.com>
2016-12-30 17:08:51 -0700

    api map

--- dist/views/bundle.js
+++ dist/views/bundle.js
@@ -66,6 +66,37 @@ angular.module('snowrider').service('mainService', function ($http) {
       return response.data.results;
     });
   };
+
+  var map;
+  var service;
+  var infowindow;
+
+  this.initialize = function () {
+    var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+    var request = {
+      location: pyrmont,
+      radius: '500',
+      types: ['store']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+  };
+
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        createMarker(results[i]);
+      }
+    }
+  }
 });
 'use strict';
 
@@ -136,5 +167,7 @@ angular.module('snowrider').controller('searchCtrl', function ($scope, mainServi
       $scope.resorts = results;
     });
   };
+
+  mainService.initialize();
 });
 //# sourceMappingURL=bundle.js.map

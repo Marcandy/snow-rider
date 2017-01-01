2249196750cb6ebfc75ad7fc88d08c9dd893aae6
marcandy <marcandy.nj@gmail.com>
2017-01-01 01:25:03 -0700

    menu

--- views/menu/menuDirective.js
+++ views/menu/menuDirective.js
@@ -18,10 +18,12 @@ angular.module('snowrider')
             link: function(scope, elem, attrs) { //elem attribute was different, so it was not applying
 
               // Initialize collapse button
-            $(".button-collapse").sideNav();
+            // $(".button-collapse").sideNav();
             // Initialize collapsible (uncomment the line below if you use the dropdown variation)
             //$('.collapsible').collapsible();
-
+            elem.on('click', function () {
+              $('.button-collapse').sideNav('show');
+            })
             $('.button-collapse').sideNav({
                 menuWidth: 300, // Default is 240
                 edge: 'right', // Choose the horizontal origin
@@ -30,7 +32,14 @@ angular.module('snowrider')
               }
             );
 
-            
+            // Show sideNav
+  // $('.button-collapse').sideNav('show');
+  // // Hide sideNav
+  // $('.button-collapse').sideNav('hide');
+  // // Destroy sideNav
+  // $('.button-collapse').sideNav('destroy')
+
+
             }
 
         }

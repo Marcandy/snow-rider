4c3359213a086c3fa36705dc2cdd644bc2868064
marcandy <marcandy.nj@gmail.com>
2017-01-02 01:55:34 -0700

    gears

--- dist/views/bundle.js
+++ dist/views/bundle.js
@@ -298,6 +298,33 @@ angular.module('snowrider').controller('guidesCtrl', function ($scope, $sce) {
 });
 'use strict';
 
+angular.module('snowrider').controller('jumboCtrl', function ($scope, $sce) {
+    $scope.vid = $sce.trustAsResourceUrl('../img/jumbo.mp4');
+}).directive('jumboDirective', function () {
+
+    return {
+        restrict: 'EA',
+
+        templateUrl: './views/jumbo/jumbo.html',
+
+        // scope: {
+        //     // lesson: '=',
+        //     // datAlert: '&'
+        // },
+
+        // controller: function($scope) {
+        //
+        // },
+
+        link: function link(scope, elem, attrs) {//elem attribute was different, so it was not applying
+
+
+        }
+
+    };
+});
+'use strict';
+
 angular.module('snowrider').directive('menuDirective', function () {
 
     return {
@@ -336,33 +363,6 @@ angular.module('snowrider').directive('menuDirective', function () {
             // $('.button-collapse').sideNav('hide');
             // // Destroy sideNav
             // $('.button-collapse').sideNav('destroy')
-
-        }
-
-    };
-});
-'use strict';
-
-angular.module('snowrider').controller('jumboCtrl', function ($scope, $sce) {
-    $scope.vid = $sce.trustAsResourceUrl('../img/jumbo.mp4');
-}).directive('jumboDirective', function () {
-
-    return {
-        restrict: 'EA',
-
-        templateUrl: './views/jumbo/jumbo.html',
-
-        // scope: {
-        //     // lesson: '=',
-        //     // datAlert: '&'
-        // },
-
-        // controller: function($scope) {
-        //
-        // },
-
-        link: function link(scope, elem, attrs) {//elem attribute was different, so it was not applying
-
 
         }
 

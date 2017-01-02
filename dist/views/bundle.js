739fc7d076c0233e53f8948f8477ec6d40a69936
marcandy <marcandy.nj@gmail.com>
2017-01-02 01:15:42 -0700

    gears

--- dist/views/bundle.js
+++ dist/views/bundle.js
@@ -207,83 +207,85 @@ angular.module('snowrider').service('mapService', function ($http, mainService) 
 
 angular.module('snowrider').directive('gearDirective', function () {
 
-    return {
-        restrict: 'EA',
+  return {
+    restrict: 'EA',
 
-        templateUrl: './views/gear/gearDirective.html',
+    templateUrl: './views/gear/gearDirective.html',
 
-        scope: {
-            // lesson: '=',
-            // datAlert: '&'
-        },
+    scope: {
+      // lesson: '=',
+      // datAlert: '&'
+    },
 
-        controller: function controller($scope) {},
+    controller: function controller($scope) {
+      $scope.gears = gears;
 
-        link: function link(scope, elem, attrs) {
-            //elem attribute was different, so it was not applying
+      $(document).ready(function () {
+        $('.carousel').carousel();
+      });
+    },
 
-            $('a[href*="#"]:not([href="#"])').click(function () {
-                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                    var target = $(this.hash);
-                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                    if (target.length) {
-                        $('html, body').animate({
-                            scrollTop: target.offset().top
-                        }, 1000);
-                        return false;
-                    }
-                }
-            });
+    link: function link(scope, elem, attrs) {
+      //elem attribute was different, so it was not applying
 
-            // scope.getSchedule.then(function (response) {
-            //   scope.schedule = response.data;
-            //
-            //   for (var i = 0; i < response.data.length; i++) {
-            //
-            //     if (response.data[i].lesson === scope.lesson) {
-            //       scope.lessonDay = response.data[i].weekday
-            //       elem.css("text-decoration", "line-through");
-            //       return;
-            //     }
-            //   }
-            // })
+      $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
         }
+      });
 
-    };
+      // scope.getSchedule.then(function (response) {
+      //   scope.schedule = response.data;
+      //
+      //   for (var i = 0; i < response.data.length; i++) {
+      //
+      //     if (response.data[i].lesson === scope.lesson) {
+      //       scope.lessonDay = response.data[i].weekday
+      //       elem.css("text-decoration", "line-through");
+      //       return;
+      //     }
+      //   }
+      // })
+    }
+
+  };
 });
-'use strict';
 
-angular.module('snowrider').controller('guidesCtrl', function ($scope, $sce) {
+var gears = [{
+  img: '../../img/gear/Moment.jpg',
+  desc: 'Our most powerful board built for wide-open freeriding, the full-camber Moment Generator thrives on mandatory cliffs, exposed traverses, and butt-clenchingly steep chutes.',
+  url: 'http://deviationusa.com/product/moment-generator/'
 
-  $scope.val = false;
-});
-'use strict';
+}, {
+  img: '../../img/gear/alpineglow.png',
+  desc: 'Top Sheet Designs',
+  url: 'http://deviationusa.com/product/moment-generator/'
+}, {
+  img: '../../img/gear/wood.png',
+  desc: 'Wood Veneer - Marbled Ako',
+  url: 'https://www.wagnerskis.com/gallery/'
+}, {
+  img: '../../img/gear/skatingX.png',
+  desc: 'Cross country set: ski, boots and poles',
+  url: 'http://pistetopowder.com/rental/'
 
-angular.module('snowrider').controller('jumboCtrl', function ($scope, $sce) {
-    $scope.vid = $sce.trustAsResourceUrl('../img/jumbo.mp4');
-}).directive('jumboDirective', function () {
+}, {
+  img: '../../img/gear/ABSp.jpg',
+  desc: 'ABS P.Ride (Mit Schaufel und Sonde)',
+  url: 'http://pistetopowder.com/rental/'
 
-    return {
-        restrict: 'EA',
-
-        templateUrl: './views/jumbo/jumbo.html',
-
-        // scope: {
-        //     // lesson: '=',
-        //     // datAlert: '&'
-        // },
-
-        // controller: function($scope) {
-        //
-        // },
-
-        link: function link(scope, elem, attrs) {//elem attribute was different, so it was not applying
-
-
-        }
-
-    };
-});
+}, {
+  img: '../../img/gear/headset.jpg',
+  desc: 'Perfect fitting ski helmet of Cébé in lime and blue',
+  url: 'https://www.vaola.co.uk/p/cebe-fireball-ski-helmet-lime-blue-2378026.html'
+}];
 'use strict';
 
 angular.module('snowrider').directive('menuDirective', function () {
@@ -324,6 +326,43 @@ angular.module('snowrider').directive('menuDirective', function () {
             // $('.button-collapse').sideNav('hide');
             // // Destroy sideNav
             // $('.button-collapse').sideNav('destroy')
+
+        }
+
+    };
+});
+'use strict';
+
+angular.module('snowrider').controller('guidesCtrl', function ($scope, $sce) {
+
+  $scope.val = false;
+
+  $(document).ready(function () {
+    $('.parallax').parallax();
+  });
+});
+'use strict';
+
+angular.module('snowrider').controller('jumboCtrl', function ($scope, $sce) {
+    $scope.vid = $sce.trustAsResourceUrl('../img/jumbo.mp4');
+}).directive('jumboDirective', function () {
+
+    return {
+        restrict: 'EA',
+
+        templateUrl: './views/jumbo/jumbo.html',
+
+        // scope: {
+        //     // lesson: '=',
+        //     // datAlert: '&'
+        // },
+
+        // controller: function($scope) {
+        //
+        // },
+
+        link: function link(scope, elem, attrs) {//elem attribute was different, so it was not applying
+
 
         }
 
--- views/guides/guidesCtrl.js
+++ views/guides/guidesCtrl.js
@@ -2,4 +2,9 @@ angular.module('snowrider')
   .controller('guidesCtrl', function ($scope, $sce) {
 
     $scope.val = false;
+
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+        
   })
